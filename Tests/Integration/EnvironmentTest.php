7563bd7e354ec3e0587f3faf41d456dce18f3fe1
Josh Pollock <josh@calderawp.com>
2018-09-18 18:41:28 -0400

    setup integration tests

---
+++ Tests/Integration/EnvironmentTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace calderawp\CalderaPay\WpClient\Tests\Integration;
+
+/**
+ * Test that integration test dependencies work
+ */
+class EnvironmentTest extends IntegrationTestCase
+{
+
+	/**
+	 * Make sure WordPress functions works
+	 */
+	public function testFunctions()
+	{
+		$this->assertTrue(function_exists( 'do_action') );
+	}
+
+    /**
+     * Make sure WordPress database works
+     */
+	public function testDatabase()
+    {
+	    $id = wp_insert_post(['post_title' => 'lol', 'post_content' => 'lol', 'post_type' => 'page' ] );
+	    $this->assertTrue( is_numeric( $id ) );
+
+    }
+}
